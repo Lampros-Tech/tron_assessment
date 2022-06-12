bf4cd8c976a3fa003cd2682da35679ed67cba79a
Akash114 <akash.lamprostech@gmail.com>
2022-06-12 15:36:36 +0530

    bot error resolved

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,3 +1,4 @@
+from asyncio.windows_events import INFINITE
 from click import command
 import telebot
 import os
@@ -5,8 +6,12 @@ import time
 from yaml import load
 from dotenv import load_dotenv
 load_dotenv()
+
 API_KEY = os.getenv("API_KEY")
+
+print(API_KEY)
 bot = telebot.TeleBot(API_KEY)
+bot.config['api_key'] = API_KEY
 for i in range(10):
     bot.send_message("-1001778640424", i)
-bot.polling()
+bot.poll()
